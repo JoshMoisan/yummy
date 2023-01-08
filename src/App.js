30115d7f8f0afcfd73e070ac34b39f713dd30b5a
joshmoisan <josh.moisan@hotmail.fr>
2023-01-08 11:26:11 -0500

    movie searching engine exercice

--- src/App.js
+++ src/App.js
@@ -1,74 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
+
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=9bd827d'
 
 const App = () => {
-  const [counterA, setCounterA] = useState(10);
-  const [counterB, setCounterB] = useState(10);
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    setMovies(data.Search)
+    console.log(data.Search)
+  }
 
+  useEffect(() => {
+    searchMovies('movie')
+  }, []);
 
   return (
-      <div className="App">
-        <h1>Big Game</h1>
-        <br/>
-        <div className='header'>
-          <div className='playerA'>
-            <button onClick={() => setCounterA((prevCount) => prevCount + 1)}>+</button>
-            <h4>{counterA}</h4>
-            <button onClick={() => setCounterA((prevCount) => prevCount - 1)}>{counterA < 0 ? "Neg" : "-"}</button>
-          </div>
+    <div className="app">
+      <h1>Movie Engine</h1>
 
-          <div className='playerB'>
-            <button onClick={() => setCounterB((prevCount) => prevCount + 1)}>+</button>
-            <h4>{counterB}</h4>
-            <button onClick={() => setCounterB((prevCount) => prevCount - 1)}>{counterB < 0 ? "Neg" : "-"}</button>
-          </div>
-        </div>
-
-      <table className='grid'>
-        <tr>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th>4</th>
-          <th>5</th>
-        </tr>
-        <tr>
-          <th>7</th>
-          <th>8</th>
-          <th>9</th>
-          <th>10</th>
-          <th>11</th>
-        </tr>
-        <tr>
-          <th>12</th>
-          <th>13</th>
-          <th>14</th>
-          <th>15</th>
-          <th>16</th>
-        </tr>
-        <tr>
-          <th>17</th>
-          <th>18</th>
-          <th>19</th>
-          <th>20</th>
-          <th>21</th>
-        </tr>
-        <tr>
-          <th>22</th>
-          <th>23</th>
-          <th>24</th>
-          <th>25</th>
-          <th>26</th>
-        </tr>
-      </table>
+      <div className="search">
+        <input
+          placeholder='Type here'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyUp={searchTerm.length >= 5 ? () => searchMovies(searchTerm) : ''}
+          type="text"
+         />
+         <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+          />
       </div>
 
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie ={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies found</h2>
+          </div>
+        )}
+
+    </div>
   );
 }
-
-
-
 
 export default App;
